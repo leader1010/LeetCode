a84600dce9cb162d7867105db0249e7fbfbfd20e
leader <930327366@qq.com>
2023-03-11 00:54:06 +0800

    全排列  回溯 剪枝

--- 47. 全排列 II.py
+++ 47. 全排列 II.py
@@ -1,18 +1,22 @@
 import copy
+
+
 class Solution:
     def permuteUnique(self, nums: list):
         res = []
         temp = []
-        visited = [False for i in range(len(nums))]
+        visited = [False for _ in range(len(nums))]
+        nums.sort()
 
         def dfs(nums, res, temp):
             if len(temp) == len(nums):
-                if temp not in res:
-                    res.append(copy.deepcopy(temp))
+                # if temp not in res:
+                res.append(copy.deepcopy(temp))
                 return
 
-
             for i in range(len(nums)):
+                if visited[i - 1] == False and i > 0 and nums[i - 1] == nums[i]:
+                    continue
                 if not visited[i]:
                     visited[i] = True
                     temp.append(nums[i])
@@ -20,19 +24,11 @@ class Solution:
                     visited[i] = False
                     temp.pop()
 
-
         dfs(nums, res, temp)
         return res
 
+
 if __name__ == '__main__':
     s = Solution()
-    res = s.permuteUnique([1, 2, 3])
+    res = s.permuteUnique([1, 1, 3])
     print(list(res))
-
-
-
-
-
-
-
-
